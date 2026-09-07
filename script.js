89709f0457610b654f51e952e35f4c3fc866c4dd
unknown <jothambeke@gmail.com>
2026-09-07 11:16:13 +0000

    modification de l'interface

--- script.js
+++ script.js
@@ -1,93 +1,21 @@
-console.log("Portfolio chargé avec succès");// ================= MENU MOBILE =================
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("nav");
 
-const menu = document.querySelector(".menu");
-const nav = document.querySelector("nav");
+menuBtn.addEventListener("click", () => {
 
-menu.addEventListener("click", () => {
     nav.classList.toggle("active");
 
-    const icon = menu.querySelector("i");
-
-    if (nav.classList.contains("active")) {
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-xmark");
-    } else {
-        icon.classList.remove("fa-xmark");
-        icon.classList.add("fa-bars");
-    }
 });
 
-// ================= FERMER LE MENU APRÈS UN CLIC =================
 
-const links = document.querySelectorAll("nav a");
+const links = nav.querySelectorAll("a");
 
 links.forEach(link => {
+
     link.addEventListener("click", () => {
+
         nav.classList.remove("active");
 
-        const icon = menu.querySelector("i");
-        icon.classList.remove("fa-xmark");
-        icon.classList.add("fa-bars");
     });
-});
-
-// ================= NAVBAR ACTIVE =================
-
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll("nav ul li a");
-
-window.addEventListener("scroll", () => {
-
-    let current = "";
-
-    sections.forEach(section => {
-
-        const sectionTop = section.offsetTop - 120;
-        const sectionHeight = section.offsetHeight;
-
-        if (pageYOffset >= sectionTop) {
-            current = section.getAttribute("id");
-        }
-
-    });
-
-    navLinks.forEach(link => {
-
-        link.classList.remove("active");
-
-        if (link.getAttribute("href") === "#" + current) {
-            link.classList.add("active");
-        }
-
-    });
-
-});
-
-// ================= APPARITION AU SCROLL =================
-
-const observer = new IntersectionObserver((entries) => {
-
-    entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-
-            entry.target.classList.add("show");
-
-        }
-
-    });
-
-}, {
-    threshold: 0.2
-});
-
-const hiddenElements = document.querySelectorAll(
-    ".skill-box, .project-card, .timeline-item, .about-cards div"
-);
-
-hiddenElements.forEach(el => {
-
-    el.classList.add("hidden");
-    observer.observe(el);
 
 });
